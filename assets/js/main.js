de49768d99c806a34668abddae43bb6c17518459
fabio <parisifabio98@gmail.com>
2021-03-01 19:23:17 +0100

    calc biglietto treno v1.2 + css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 //chiedo il numero di km che l'utente vuole fare
 var km = prompt("quanti kilometri vuoi fare?", 10);
+document.getElementById("km").innerHTML = km;
+
 if(isNaN(km)){
 	//richiedo il numero nel caso abbia sbagliato a scrivere, per farlo bene sarebbe meglio usare un while ma mi sono limitato al if
 	km = prompt("questo non è un numero, reinserisci quanti kilometri vuoi fare", 10);
 }
 //chiedo l'eta del utente
 var eta = prompt("quanti anni hai?", 20);
+document.getElementById("eta").innerHTML = eta;
+
 if(isNaN(eta)){
 	//richiedo il numero nel caso abbia sbagliato a scrivere, per farlo bene sarebbe meglio usare un while ma mi sono limitato al if
 	eta = prompt("questo non è un numero, reinserisci quanti anni hai", 20);
@@ -19,15 +23,19 @@ console.log(fullPrice);
 if(eta < 18){
 	//per fare lo sconto del 20% ho fatto *80/100
 	lowPrice = price = ((fullPrice * 80) / 100).toFixed(2);
+	sale = "20%"
 }
 else if(eta > 65){
 	//per fare lo sconto del 40% ho fatto *60/100
 	lowestPrice = price = ((fullPrice * 60) / 100).toFixed(2);
+	sale = "40%"
 }
 else{
 	//per il biglietto a prezzo pieno
 	price = fullPrice;
+	sale = "0%"
 }
+document.getElementById("sale").innerHTML = sale;
 console.log(price);
 //scrivo il prezzo del biglietto sul html
 document.getElementById("price").innerHTML = price;
